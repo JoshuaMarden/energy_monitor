a64da439b609d3b0c9a0f0e303d82cd885fae855
HighestAuto <jonathan.at.spence@hotmail.com>
2024-08-13 13:04:59 +0100

    lambda roles and extract carbon lambda

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -11,6 +11,25 @@ provider "aws" {
   region  = var.AWS_REGION
 }
 
+# Lambda role to allow interactions
+resource "aws_iam_role" "lambda" {
+  name = "c12-energy-tracker-lambda-role"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AWSLambda_FullAccess", "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+  max_session_duration = 3600
+  path                  = "/"
+}
+
+# S3 bucket
 resource "aws_s3_bucket" "extract" {
   bucket = "c12-energy-tracker"
   force_destroy = true
@@ -19,6 +38,8 @@ resource "aws_s3_bucket_notification" "trigger" {
   bucket = aws_s3_bucket.extract.id
   eventbridge = true
 }
+
+# ECS task deifinition and trigger
 resource "aws_cloudwatch_event_rule" "to_tl" {
   name        = "c12-energy_consumption"
   force_destroy = true
@@ -34,7 +55,6 @@ resource "aws_cloudwatch_event_rule" "to_tl" {
     }
   })
 }
-
 resource "aws_ecs_task_definition" "energy-pipeline" {
   family = "c12-energy-pipeline"
   cpu = 1024
@@ -85,18 +105,18 @@ resource "aws_cloudwatch_event_target" "trigger-pipeline" {
 
     network_configuration {
       subnets = ["subnet-058f02e41ee6a5439", "subnet-0c459ebb007081668", "subnet-0ff947058bbc1165d"]
-      assign_public_ip = true	
-                                              
+      assign_public_ip = true	                            
     }
   }
 }
 
+# Lambda running production extract
 resource "aws_lambda_function" "extract_production" {
   architectures                      = ["x86_64"]
   function_name                      = "c12-energy-extract-production"
   image_uri                          = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c12-energy-extract:latest"
   package_type                       = "Image"
-  role                               = "arn:aws:iam::129033205317:role/c12-energy-tracker-lambda-role"
+  role                               = aws_iam_role.lambda.arn
   environment {
     variables = {
       DB_HOST     = var.DB_HOST
@@ -113,8 +133,28 @@ resource "aws_lambda_function" "extract_production" {
     log_group             = "/aws/lambda/c12-energy-production"
   }
 }
-
-resource "aws_scheduler_schedule" "lambda_schedule" {
+resource "aws_iam_role" "extract_production" {
+  name = "c12-energy-extract-production"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Condition = {
+        StringEquals = {
+          "aws:SourceAccount" = "129033205317"
+        }
+      }
+      Effect = "Allow"
+      Principal = {
+        Service = "scheduler.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  managed_policy_arns   = ["arn:aws:iam::129033205317:policy/service-role/Amazon-EventBridge-Scheduler-Execution-Policy-9ddb4bfa-cc6b-405a-9275-03df4997c2e8"]
+  max_session_duration  = 3600
+  path                  = "/service-role/"
+}
+resource "aws_scheduler_schedule" "lambda_schedule_30_min" {
   group_name                   = "default"
   name                         = "c12-energy-extract-production"
   schedule_expression          = "cron(30 * * * ? *)"
@@ -126,7 +166,71 @@ resource "aws_scheduler_schedule" "lambda_schedule" {
   }
   target {
     arn      = aws_lambda_function.extract_production.arn
-    role_arn = "arn:aws:iam::129033205317:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_cd0fa4fefc"
+    role_arn = aws_iam_role.extract_production.arn
+    retry_policy {
+      maximum_event_age_in_seconds = 86400
+      maximum_retry_attempts       = 185
+    }
+  }
+}
+
+# Lambda running carbon extract
+resource "aws_lambda_function" "extract_carbon" {
+  architectures                      = ["x86_64"]
+  function_name                      = "c12-energy-extract-carbon"
+  image_uri                          = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c12-energy-extract-carbon:latest"
+  package_type                       = "Image"
+  role                               = aws_iam_role.lambda.arn
+  environment {
+    variables = {
+      DB_HOST     = var.DB_HOST
+      DB_NAME     = var.DB_NAME
+      DB_PASSWORD = var.DB_PASSWORD
+      DB_PORT     = var.DB_PORT
+      DB_USER     = var.DB_USER
+      ACCESS_KEY_ID = var.AWS_ACCESS_KEY
+      SECRET_ACCESS_KEY = var.AWS_SECRET_KEY
+    }
+  }
+  logging_config {
+    log_format            = "Text"
+    log_group             = "/aws/lambda/c12-energy-carbon"
+  }
+}
+resource "aws_iam_role" "extract_carbon" {
+  name = "c12-energy-extract-carbon"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Condition = {
+        StringEquals = {
+          "aws:SourceAccount" = "129033205317"
+        }
+      }
+      Effect = "Allow"
+      Principal = {
+        Service = "scheduler.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  managed_policy_arns   = ["arn:aws:iam::129033205317:policy/service-role/Amazon-EventBridge-Scheduler-Execution-Policy-9ddb4bfa-cc6b-405a-9275-03df4997c2e8"]
+  max_session_duration  = 3600
+  path                  = "/service-role/"
+}
+resource "aws_scheduler_schedule" "lambda_schedule_1_day" {
+  group_name                   = "default"
+  name                         = "c12-energy-extract-carbon"
+  schedule_expression          = "cron(0 0 * * ? *)"
+  schedule_expression_timezone = "UTC"
+  state                        = "ENABLED"
+  flexible_time_window {
+    maximum_window_in_minutes = 15
+    mode                      = "FLEXIBLE"
+  }
+  target {
+    arn      = aws_lambda_function.extract_carbon.arn
+    role_arn = aws_iam_role.extract_carbon.arn
     retry_policy {
       maximum_event_age_in_seconds = 86400
       maximum_retry_attempts       = 185
