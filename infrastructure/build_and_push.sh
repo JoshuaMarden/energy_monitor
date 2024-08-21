5ab04aea3a4858fc42c21e90cd9126aac9d9e0bf
Joshua Jack Marden <joshuasigma@Joshuas-MacBook-Pro.local>
2024-08-21 11:01:53 +0100

    add line for servie restart with new images

--- infrastructure/build_and_push.sh
+++ infrastructure/build_and_push.sh
@@ -11,7 +11,7 @@
 # Variables
 REGION="eu-west-2"
 ACCOUNT_ID="129033205317"
-CLUSTER_NAME="c12-cluster"
+CLUSTER_NAME="c12-ecs-cluster"
 SERVICE_NAME="c12-energy-dashboard"
 REPO_PREFIX="c12-energy"
 REPO_SUFFIX=("extract" "extract-carbon" "pipeline" "dashboard")
