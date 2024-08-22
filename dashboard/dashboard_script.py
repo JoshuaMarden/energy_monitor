5585b5f0c7cb50dddb11b80e2f522167b1e3dfaa
Polariod <85454474+Polariod@users.noreply.github.com>
2024-08-22 10:47:07 +0100

    tabs added as well as filters for time

--- dashboard/dashboard_script.py
+++ dashboard/dashboard_script.py
@@ -3,6 +3,8 @@ import os
 import streamlit as st
 import psycopg2
 import altair as alt
+import datetime as dt
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -33,9 +35,11 @@ class DataLoader:
         demand_query = "SELECT * FROM Demand"
         gen_query = "SELECT * FROM Generation"
         carbon_query = "SELECT * FROM Carbon"
+        piechart_query = "SELECT * FROM generation_percent"
         demand_df = pd.read_sql_query(demand_query, conn)
         gen_df = pd.read_sql_query(gen_query, conn)
         carbon_df = pd.read_sql_query(carbon_query, conn)
+        pie_df = pd.read_sql_query(piechart_query, conn)
 
         if conn:
             conn.close()
@@ -68,6 +72,7 @@ class dashboard:
         return alt.Chart(df_fuel_types[["publish_time", "generated", "fuel_type"]]).mark_line().encode(
             x=alt.X('publish_time:T', title='Time'), y=alt.Y('generated:Q', title='Energy Generated MW'),
             color="fuel_type").properties(width=1000, height=1000)
+    # def generate_piechart(pie_df):
 
     def intensity_factors_df(self) -> alt.Chart:
         intensity_factors = {'Energy Source': ['Biomass', 'Coal', 'Dutch Imports', 'French Imports', 'Gas (Combined Cycle)', 'Gas (Open Cycle)',
@@ -118,21 +123,26 @@ class dashboard:
     def generate_dashboard(self, gen_df: pd.DataFrame, demand_df: pd.DataFrame, carbon_df: pd.DataFrame):
         st.set_page_config(layout="wide")
         st.title("Energy Monitor")
-        with st.container():
-            st.header("Carbon Intensity Forecast")
-            col1, col2 = st.columns(2)
-            with col1:
-                st.altair_chart(self.carbon_plots(carbon_df))
-                carbon_container = st.container(border=True)
-                with carbon_container:
-                    low_col, high_col = carbon_container.columns(2)
-                    with low_col:
-                        st.metric(label="Lowest Carbon Footprint at:", value=f"{self.time_of_lowest_carbon(
-                            carbon_df)}", delta=f"{min(carbon_df["forecast"].values)}")
-                    with high_col:
-                        st.metric(label="Highest Carbon Footprint at:", value=f"{
-                            self.time_of_highest_carbon(carbon_df)}", delta=f"{max(carbon_df["forecast"].values)}", delta_color="inverse")
-                with st.expander("Join our mail list to be informed on the greenest time to charge your EV"):
+        carbon_tab, generation_tab = st.tabs(
+            ["Carbon Impact", "Energy Generation"])
+
+        with carbon_tab:
+            with st.container():
+                st.header("Carbon Intensity Forecast")
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.altair_chart(self.carbon_plots(carbon_df))
+                    carbon_container = st.container(border=True)
+                    with carbon_container:
+                        low_col, high_col = carbon_container.columns(2)
+                        with low_col:
+                            st.metric(label="Lowest Carbon Footprint at:", value=f"{self.time_of_lowest_carbon(
+                                carbon_df)}", delta=f"{min(carbon_df["forecast"].values)}")
+                        with high_col:
+                            st.metric(label="Highest Carbon Footprint at:", value=f"{
+                                self.time_of_highest_carbon(carbon_df)}", delta=f"{max(carbon_df["forecast"].values)}", delta_color="inverse")
+                    st.header(
+                        "Join our mail list to be informed on the greenest time to charge your EV")
                     email_form = st.form("email_form")
                     name = email_form.text_input("Name")
                     email = email_form.text_input("Email")
@@ -144,41 +154,48 @@ class dashboard:
 
                     email_form.form_submit_button("Submit")
 
-            with col2:
-                st.header("What is Carbon Intensity?")
-                st.write("The carbon intensity of electricity is a measure of how much CO2 emissions are produced per kilowatt hour of electricity consumed.The carbon intensity of electricity is sensitive to small changes in carbon-intensive generation. Carbon intensity varies due to changes in electricity demand, low carbon generation (wind, solar, hydro, nuclear, biomass) and conventional generation.")
-                with st.expander("Find out more"):
-                    Method_tab, Factors_tab = st.tabs(
-                        ["Methodology", "Intensity Factors"])
-                    with Method_tab:
-                        st.write("The demand and generation by fuel type (gas, coal, wind, nuclear, solar etc.) for each region is forecast several days ahead at 30-min temporal resolution using an ensemble of state-of-the-art supervised Machine Learning (ML) regression models. An advanced model ensembling technique is used to blend the ML models to generate a new optimised meta-model. The forecasts are updated every 30 mins using a nowcasting technique to adjust the forecasts a short period ahead. The carbon intensity factors are applied to each technology type for each import generation mix to calculate the forecast")
-                    with Factors_tab:
-                        st.table(self.intensity_factors_df())
+                with col2:
+                    st.header("What is Carbon Intensity?")
+                    st.write("The carbon intensity of electricity is a measure of how much CO2 emissions are produced per kilowatt hour of electricity consumed.The carbon intensity of electricity is sensitive to small changes in carbon-intensive generation. Carbon intensity varies due to changes in electricity demand, low carbon generation (wind, solar, hydro, nuclear, biomass) and conventional generation.")
+                    with st.expander("Find out more"):
+                        Method_tab, Factors_tab = st.tabs(
+                            ["Methodology", "Intensity Factors"])
+                        with Method_tab:
+                            st.write("The demand and generation by fuel type (gas, coal, wind, nuclear, solar etc.) for each region is forecast several days ahead at 30-min temporal resolution using an ensemble of state-of-the-art supervised Machine Learning (ML) regression models. An advanced model ensembling technique is used to blend the ML models to generate a new optimised meta-model. The forecasts are updated every 30 mins using a nowcasting technique to adjust the forecasts a short period ahead. The carbon intensity factors are applied to each technology type for each import generation mix to calculate the forecast")
+                        with Factors_tab:
+                            st.table(self.intensity_factors_df())
+        with generation_tab:
+            show_all_data = st.checkbox('Show all historical data')
+            if not show_all_data:
+                current_time = dt.datetime.now()
+                last_24_hours = current_time - timedelta(hours=24)
+                filtered_df = gen_df[gen_df['publish_time'] >= last_24_hours]
+            else:
+                filtered_df = gen_df
+            with st.container():
+                st.header("Energy Generation by Fuel Type")
+                col1, col2 = st.columns([3, 1])
+                with col1:
+                    st.altair_chart(self.generation_fuel_type(filtered_df))
+                with col2:
+                    with st.expander("Fuel Type Information"):
+                        st.write("""
+    - **BIOMASS**: Organic materials used as a renewable energy source. It includes plant materials and animal waste that can be burned or converted to biofuels.
+    - **CCGT (Combined Cycle Gas Turbine)**: A form of highly efficient energy generation that combines a gas-fired turbine with a steam turbine. 
+    - **COAL**: Traditional fossil fuel used for generating electricity through combustion.
+    - **NPSHYD (Non-Pumped Storage Hydro)**: Refers to the conventional hydroelectric generation where water flows through turbines to generate power, with no pumped storage components.
+    - **NUCLEAR**: Power generated using nuclear reactions.Provides a stable base-load energy supply.
+    - **OCGT (Open Cycle Gas Turbine)**: A type of gas power plant where air is compressed, mixed with fuel, and burned in a simple cycle. It's less efficient than CCGT but is often quicker to start and stop.
+    - **OIL**: Oil-fired power plants burn petroleum or its derivatives to generate electricity. Similar to coal, it has large environmental impacts. 
+    - **OTHER**: This category usually encompasses any generation sources not specifically listed, which could include experimental or less common technologies like tidal or certain types of bio-gas plants.
+    - **PS (Pumped Storage)**: A type of hydropower generation where water is pumped to a higher elevation during low demand periods and released through turbines for electricity during high demand.
+    - **WIND**: The use of wind turbines to convert wind energy into electricity. It's a clean, renewable source increasingly used worldwide, particularly in areas with strong, consistent winds.
+    """)
 
-        with st.container():
-            st.header("Energy Generation by Fuel Type")
-            col1, col2 = st.columns([3, 1])
-            with col1:
-                st.altair_chart(self.generation_fuel_type(gen_df))
-            with col2:
-                with st.expander("Fuel Type Information"):
-                    st.write("""
-- **BIOMASS**: Organic materials used as a renewable energy source. It includes plant materials and animal waste that can be burned or converted to biofuels.
-- **CCGT (Combined Cycle Gas Turbine)**: A form of highly efficient energy generation that combines a gas-fired turbine with a steam turbine. 
-- **COAL**: Traditional fossil fuel used for generating electricity through combustion.
-- **NPSHYD (Non-Pumped Storage Hydro)**: Refers to the conventional hydroelectric generation where water flows through turbines to generate power, with no pumped storage components.
-- **NUCLEAR**: Power generated using nuclear reactions.Provides a stable base-load energy supply.
-- **OCGT (Open Cycle Gas Turbine)**: A type of gas power plant where air is compressed, mixed with fuel, and burned in a simple cycle. It's less efficient than CCGT but is often quicker to start and stop.
-- **OIL**: Oil-fired power plants burn petroleum or its derivatives to generate electricity. Similar to coal, it has large environmental impacts. 
-- **OTHER**: This category usually encompasses any generation sources not specifically listed, which could include experimental or less common technologies like tidal or certain types of bio-gas plants.
-- **PS (Pumped Storage)**: A type of hydropower generation where water is pumped to a higher elevation during low demand periods and released through turbines for electricity during high demand.
-- **WIND**: The use of wind turbines to convert wind energy into electricity. It's a clean, renewable source increasingly used worldwide, particularly in areas with strong, consistent winds.
-""")
-
-        with st.container():
-            st.header("Where is your energy coming from ? ")
-            st.write("Interconnectors are high-voltage links allowing electricity to flow between regions or countries, providing crucial flexibility and reliability to power supplies. They help balance energy demand and supply, integrate renewable energy sources, and enhance grid resilience and sustainability. This leads to more stable electricity prices and a more reliable power supply for everyone.")
-            st.altair_chart(self.generation_interconnection(gen_df))
+            with st.container():
+                st.header("Where is your energy coming from ? ")
+                st.write("Interconnectors are high-voltage links allowing electricity to flow between regions or countries, providing crucial flexibility and reliability to power supplies. They help balance energy demand and supply, integrate renewable energy sources, and enhance grid resilience and sustainability. This leads to more stable electricity prices and a more reliable power supply for everyone.")
+                st.altair_chart(self.generation_interconnection(filtered_df))
 
 
 if __name__ == "__main__":
