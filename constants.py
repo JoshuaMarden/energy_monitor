91bd1fb1fb4e147400e12f23e5e78451b1134b21
Joshua Jack Marden <joshuasigma@Joshuas-MBP.lan>
2024-10-15 10:22:02 +0100

    bringing project back online

--- constants.py
+++ constants.py
@@ -14,6 +14,8 @@ class Constants:
     RAW_DEMAND_DATA_NAME = "raw_demand_data.feather"
     RAW_PRICE_DATA_NAME = "raw_cost_data.feather"
     RAW_CARBON_DATA_NAME = "raw_carbon_data.feather"
+    RAW_PIECHART_DATA_NAME = "raw_piechart_data.feather"
+
 
     # Paths to local storage for data
     RAW_GENERATION_DATA_PATH = DATA + RAW_GENERATION_DATA_NAME
@@ -26,11 +28,11 @@ class Constants:
     DEMAND_ENDPOINT = "https://data.elexon.co.uk/bmrs/api/v1/generation/outturn"
     PRICE_ENDPOINT = "https://data.elexon.co.uk/bmrs/api/v1/balancing/settlement/system-prices/"
     CARBON_ENDPOINT = "https://api.carbonintensity.org.uk/intensity/date"
-
+    PIECHART_ENDPOINT = "https://api.carbonintensity.org.uk/generation"
+    CARBON_ENDPOINT = "https://api.carbonintensity.org.uk/intensity/date"
+   
     # S3 Data
     S3_BUCKET = "c12-energy-tracker"
-
-    CARBON_ENDPOINT = "https://api.carbonintensity.org.uk/intensity/date"
 
     def __setattr__(self, name: str, value: Any) -> None:
         raise AttributeError(f"Can't Reassign Constant {name}")
